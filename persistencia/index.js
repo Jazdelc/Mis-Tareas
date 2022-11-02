d17d1363c65fc4bb5b88d85211730d26528fb4b7
Jazdelc <87844657+Jazdelc@users.noreply.github.com>
2022-11-02 18:19:02 -0300

    Add files via upload

---
+++ persistencia/index.js
@@ -0,0 +1,12 @@
+let nombre = "Jazmin"
+let apellido = "Sepulveda"
+const persona = {nombre, apellido}
+
+//sessionStorage.setItem(JSON.stringify({nombre: "Jazmin", apellido: "Sepulveda"}))
+//console.log(sessionStorage.getItem({nombre: "Jazmin", apellido: "Sepulveda"}))
+
+//localStorage.setItem(JSON.stringify({nombre: "Jazmin", apellido: "Sepulveda"}))
+//console.log(localStorage.getItem({nombre: "Jazmin", apellido: "Sepulveda"}))
+
+//document.cookie = "nombreCaducidad=Nombre;expires=" + new Date(2022, 8, 14, 1,13,55).toUTCString()
+//console.log(document.cookie)
