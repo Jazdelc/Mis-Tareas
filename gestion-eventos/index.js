68266ba79d402e95e9f987f656c802ca51715bd7
Jazdelc <87844657+Jazdelc@users.noreply.github.com>
2022-11-02 18:19:47 -0300

    Add files via upload

---
+++ gestion-eventos/index.js
@@ -0,0 +1,10 @@
+let boton = document.querySelector("button")
+
+boton.addEventListener("click", () => alert("Click en el botón"))
+
+$("button").click(function() {
+    console.log("Hola, estoy utilizando jQuery")
+})
+
+
+
