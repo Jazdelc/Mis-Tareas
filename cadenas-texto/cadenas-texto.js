96c1a2eef83fd027aaee3c04c906391f0ecb12da
Jazdelc <87844657+Jazdelc@users.noreply.github.com>
2022-11-05 01:30:22 -0300

    Add files via upload

---
+++ cadenas-texto/cadenas-texto.js
@@ -0,0 +1,25 @@
+let nombre = "Jazmin"
+let apellido = "Sepulveda"
+
+let estudiante = nombre + " " + apellido
+
+let estudianteMayus = "estudiante"
+console.log(estudianteMayus.toUpperCase())
+
+let estudianteMinus = "ESTUDIANTE"
+console.log(estudianteMinus.toLowerCase())
+
+let numLetras = console.log(estudiante.length)
+
+let letra1 = console.log(nombre.charAt(0))
+
+let letra2 = console.log(apellido.charAt(8))
+
+let texto = "     Jazmin Sepulveda     "
+console.log(texto.trim())
+
+let palabra = console.log(estudiante.includes(nombre))
+
+
+
+
